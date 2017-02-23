4f2f9aa27baa6b78c668ffb043cd7d1b5cf6e24a
Erik Winge <erik.winge@ub.uio.no>
2017-02-23 12:58:18 +0100

    more robust

--- adresse.py
+++ adresse.py
@@ -7,6 +7,14 @@ con = Connection("ldap.uio.no", auto_bind=True)
 base = "cn=people,dc=uio,dc=no"
 
 
+def get_input(prompt):
+    "Get a nonempty input from the user"
+    read = ""
+    while not read:
+        read = input(prompt).strip()
+    return read
+
+
 def print_file(filename):
     "Send a text file to the default Windows printer"
     import subprocess
@@ -14,9 +22,21 @@ def print_file(filename):
 #     subprocess.call(["notepad", filename])
 
 
+def print_person(entry):
+    "Print address slip for an LDAP entry"
+    filename = "address-temp.txt"
+    try:
+        with open(filename, "w") as out:
+            print(entry.cn, str(entry.postalAddress).replace("$", "\n"), sep="\n", end="\n", file=out, flush=True)
+        print_file(filename)
+        os.remove(filename)
+    except Exception as e:
+        print(e)
+
+
 def get_user():
     "Lookup a user in LDAP"
-    name = input("Name: ")
+    name = get_input("Name: ")
     search_string = "(cn =* %s)" % name
     con.search(base, search_string, attributes=["cn", "postalAddress"])
 
@@ -28,22 +48,18 @@ def get_user():
         print("Names with more than 200 matches cannot be displayed")
         return
 
-    choice = input("Select person (a aborts): ")
-    if choice == "a":
-        return
+    choice = ""
+    while not (isinstance(choice, int) and choice >= 0 and choice < len(con.entries)):
+        choice = get_input("Select person 0-%d (a aborts): " % (len(con.entries) - 1))
+        if choice == "a":
+            return
+        try:
+            choice = int(choice)
+        except ValueError:
+            pass
 
-    entry = con.entries[int(choice)]
-
-    filename = "address-temp.txt"
-
-    try:
-        with open(filename, "w") as out:
-            print(entry.cn, str(entry.postalAddress).replace("$", "\n"), sep="\n", end="\n", file=out, flush=True)
-
-        print_file(filename)
-        os.remove(filename)
-    except Exception as e:
-        print(e)
+    entry = con.entries[choice]
+    print_person(entry)
 
 
 if __name__ == '__main__':
