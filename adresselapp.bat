9dd70ba9e71eca07ecfc2c623d30f15eff1bd305
Erik Winge <erik.winge@ub.uio.no>
2024-01-18 11:26:17 +0100

    Fix batch file that starts the program on Windows

--- adresselapp.bat
+++ adresselapp.bat
@@ -1,4 +1,5 @@
 @Echo off
 REM change CHCP to UTF-8
 CHCP 65001
-Python \\kant\jus-felles\fak-felles\ujur\script\adresselapp\adresse.py
+CALL C:\Anaconda3\Scripts\activate.bat 
+Python \\kant.uio.no\jus-felles\fak-felles\ujur\script\adresselapp\adresse.py
